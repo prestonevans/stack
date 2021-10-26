dc8df35b7fbcea3fbc73c0bbf85108f1ff2761a8
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-25 21:20:43 -0600

    made queue with array and array methods

--- js/app.js
+++ js/app.js
@@ -137,77 +137,172 @@
 
 // console.log(stackStack.storage);
 
-class Stack {
-	constructor() {
-		this.storage = {};
-		this.count = 0;
-	}
-	push(el) {
-		this.count++;
-		this.storage[this.count] = el;
-	}
-	pop() {
-		if (this.count === 0) return undefined;
-		let temp = this.storage[this.count];
-		delete this.storage[this.count];
-		this.count--;
-		return temp;
-	}
-	peek() {
-		return this.storage[this.count];
-	}
-	clear() {
-		this.storage = {};
-		this.count = 0;
-		return 'cleared';
-	}
-}
+// class Stack {
+// 	constructor() {
+// 		this.storage = {};
+// 		this.count = 0;
+// 	}
+// 	push(el) {
+// 		this.count++;
+// 		this.storage[this.count] = el;
+// 	}
+// 	pop() {
+// 		if (this.count === 0) return undefined;
+// 		let temp = this.storage[this.count];
+// 		delete this.storage[this.count];
+// 		this.count--;
+// 		return temp;
+// 	}
+// 	peek() {
+// 		return this.storage[this.count];
+// 	}
+// 	clear() {
+// 		this.storage = {};
+// 		this.count = 0;
+// 		return 'cleared';
+// 	}
+// }
 
-const stack = new Stack();
-console.log(stack.pop());
-stack.push('test');
-console.log(stack.peek());
-stack.push('is');
-stack.push('this');
-stack.push('thing');
-stack.clear();
-stack.push('working?');
-console.log(stack.storage);
+// const stack = new Stack();
+// console.log(stack.pop());
+// stack.push('test');
+// console.log(stack.peek());
+// stack.push('is');
+// stack.push('this');
+// stack.push('thing');
+// stack.clear();
+// stack.push('working?');
+// console.log(stack.storage);
+
+// class Queue {
+// 	constructor() {
+// 		this.storage = {};
+// 		this.count = 0;
+// 		this.rmCounter = 0;
+// 	}
+// 	enqueue(el) {
+// 		this.count++;
+// 		this.storage[this.count] = el;
+// 	}
+// 	dequeue() {
+// 		if (this.count === this.rmCounter) return undefined;
+// 		this.rmCounter++;
+// 		let temp = this.storage[this.rmCounter];
+// 		delete this.storage[this.rmCounter];
+// 		return temp;
+// 	}
+// 	peek() {
+// 		return this.storage[this.rmCounter + 1];
+// 	}
+// 	size() {
+// 		return this.count - this.rmCounter;
+// 	}
+// }
+
+// const queue = new Queue();
+
+// console.log(queue.peek());
+// queue.enqueue('is');
+// console.log(queue.dequeue());
+// // console.log(queue.dequeue());
+// queue.enqueue('this');
+// queue.enqueue('working?');
+// console.log(queue.storage);
+// queue.enqueue('haha');
+// console.log(queue.peek());
+// console.log(queue.size());
+
+// function multiply(a, b) {
+// 	return a * b;
+// }
+// function square(num) {
+// 	return multiply(num, num);
+// }
+
+// function isSquare(num) {
+// 	return square(num) === num ** 2;
+// }
+
+// console.log(isSquare(2));
+
+// console.log('Sending request to server');
+
+// setTimeout(() => {
+// 	console.log('Here is your data bub');
+// }, 2000);
+// console.log('I am at the end of the file');
+
+// document.body.style.backgroundColor = 'red';
+
+// setTimeout(() => {
+// 	document.body.style.backgroundColor = 'orange';
+// 	setTimeout(() => {
+// 		document.body.style.backgroundColor = 'yellow';
+// 		setTimeout(() => {
+// 			document.body.style.backgroundColor = 'green';
+// 			setTimeout(() => {
+// 				document.body.style.backgroundColor = 'blue';
+// 				setTimeout(() => {
+// 					document.body.style.backgroundColor = 'indigo';
+// 					setTimeout(() => {
+// 						document.body.style.backgroundColor = 'violet';
+// 					}, 3000);
+// 				}, 3000);
+// 			}, 3000);
+// 		}, 3000);
+// 	}, 3000);
+// }, 500);
+
+// const fakeUrlCallback = (url, success, failure) => {
+// 	let delay = Math.floor(Math.random() * 2000);
+// 	setTimeout(() => {
+// 		if (delay < 1000) {
+// 			success(`You made it to ${url}`);
+// 		} else {
+// 			failure('Nah not today');
+// 		}
+// 	}, delay);
+// };
+
+// fakeUrlCallback(
+// 	'google.com',
+// 	(x) => {
+// 		console.log(x);
+// 	},
+// 	(x) => {
+// 		console.log(x);
+// 	}
+// );
 
 class Queue {
 	constructor() {
-		this.storage = {};
-		this.count = 0;
-		this.rmCounter = 0;
+		this.storage = [];
 	}
 	enqueue(el) {
-		this.count++;
-		this.storage[this.count] = el;
+		return this.storage.push(el);
 	}
 	dequeue() {
-		if (this.count === this.rmCounter) return undefined;
-		this.rmCounter++;
-		let temp = this.storage[this.rmCounter];
-		delete this.storage[this.rmCounter];
-		return temp;
+		return this.storage.shift();
 	}
 	peek() {
-		return this.storage[this.rmCounter + 1];
+		if (this.storage.length === 0) return undefined;
+		return this.storage[0];
 	}
 	size() {
-		return this.count - this.rmCounter;
+		this.storage.length;
+	}
+	clear() {
+		this.storage = [];
 	}
 }
 
-const queue = new Queue();
+const test = new Queue();
 
-console.log(queue.peek());
-queue.enqueue('is');
-console.log(queue.dequeue());
-// console.log(queue.dequeue());
-queue.enqueue('this');
-queue.enqueue('working?');
-console.log(queue.storage);
-queue.enqueue('haha');
-console.log(queue.peek());
-console.log(queue.size());
+console.log(test.enqueue('is'));
+test.enqueue('this');
+test.enqueue('working');
+console.log(test.dequeue());
+console.log(test.storage[0]);
+test.clear();
+console.log(test.peek());
+console.log(test.storage);
